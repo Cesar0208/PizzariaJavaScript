37bd236aefde54a342077fcd9f224af270de8d9d
GlauberMaximo <155695736+GlauberMaximo@users.noreply.github.com>
2025-11-05 17:18:35 -0300

    Add `sobre` das pizzas

---
+++ Pizaria-JavaScript/script.js
@@ -0,0 +1,10 @@
+let produto1 = {
+    nome: "Margherita",
+    preco: 10.99,
+    descricao: "Pizza Margherita",
+    estoque: "Em estoque"
+}
+
+const titulo1 = document.getElementById("texto1");
+titulo1.textContent = produto1.nome
+
